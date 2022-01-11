74c34c992c0ae3e3a877212406012e2ccc818f66
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-11 16:22:37 +0100

    implements PostNoteHandler for creating notes

--- chapter 4/listing-4-15-building-a-restful-api/main.go
+++ chapter 4/listing-4-15-building-a-restful-api/main.go
@@ -21,3 +21,28 @@ var noteStore = make(map[string]Note)
 
 // Variable to generate key for the collection
 var id int = 0
+
+// HTTP Post - /api/notes
+func PostNoteHandler(w http.ResponseWriter, r *http.Request) {
+	var note Note
+	// Decode the incoming Note json
+	err := json.NewDecoder(r.Body).Decode(&note)
+	if err != nil {
+		panic(err)
+	}
+
+	// Store note to noteStore
+	note.CreatedOn = time.Now()
+	id++
+	k := strconv.Itoa(id)
+	noteStore[k] = note
+
+	// HTTP response
+	j, err := json.Marshal(note)
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(j)
+}
