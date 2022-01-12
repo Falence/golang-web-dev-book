edb72ed3b57bc272594e183e90c99808e7156759
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-12 20:09:36 +0100

    implements updateNote() and deleteNote handler functions

--- chapter-5/build-a-web-app-with-templates/main.go
+++ chapter-5/build-a-web-app-with-templates/main.go
@@ -16,6 +16,7 @@ type Note struct {
 	CreatedOn time.Time
 }
 
+// View Model for edit
 type EditNote struct {
 	Note
 	Id string
@@ -30,6 +31,7 @@ var id int = 0
 
 
 var templates map[string]*template.Template
+
 // Compile view templates
 func init() {
 	if templates == nil {
@@ -87,6 +89,41 @@ func editNote(w http.ResponseWriter, r *http.Request) {
 	}
 	renderTemplate(w, "edit", "base", viewModel)
 }
+
+func updateNote(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	k := vars["id"]
+	var noteToUpd Note
+
+	if note, ok := noteStore[k]; ok {
+		r.ParseForm()
+		noteToUpd.Title = r.PostFormValue("title")
+		noteToUpd.Description = r.PostFormValue("description")
+		noteToUpd.CreatedOn = note.CreatedOn
+
+		// Delete existing item and add the updated item
+		delete(noteStore, k)
+		noteStore[k] = noteToUpd
+	} else {
+		http.Error(w, "Could not find the resource to update.", http.StatusBadRequest)
+	}
+	http.Redirect(w, r, "/", 302)
+}
+
+func deleteNote(w http.ResponseWriter, r *http.Request) {
+	//Read value from route variable
+	vars := mux.Vars(r)
+	k := vars["id"]
+	
+	// Remove from Store
+	if _, ok := noteStore[k]; ok {
+	//delete existing item
+	delete(noteStore, k)
+	} else {
+		http.Error(w, "Could not find the resource to delete.", http.StatusBadRequest)
+	}
+	http.Redirect(w, r, "/", 302)
+ }
 
 func main() {
 	r := mux.NewRouter().StrictSlash(false)
