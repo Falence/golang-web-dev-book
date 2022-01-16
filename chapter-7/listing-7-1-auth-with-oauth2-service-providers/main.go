5b46704da31d9edcb261c3c3a02dfe1844f4164c
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-16 16:44:22 +0100

    implement init() for reading configuration values from the config.jso file

--- chapter-7/listing-7-1-auth-with-oauth2-service-providers/main.go
+++ chapter-7/listing-7-1-auth-with-oauth2-service-providers/main.go
@@ -14,3 +14,24 @@ import (
 	"github.com/markbates/goth/providers/facebook"
 	"github.com/markbates/goth/providers/twitter"
 )
+
+// Struct for parsing JSON configuration
+type Configuration struct {
+	TwitterKey string
+	TwitterSecret string
+	FacebookKey string
+	FacebookSecret string
+}
+
+var config Configuration
+
+// Read configuration values from config.json
+func init() {
+	file, _ := os.Open("config.json")
+	decoder := json.NewDecoder(file)
+	config = Configuration{}
+	err := decoder.Decode(&config)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
