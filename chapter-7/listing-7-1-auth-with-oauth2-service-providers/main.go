d0bdf8b4fd372df7b2ecf55196f64310c02cad6b
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-16 16:36:47 +0100

    implementing oauth(facebook and twitter): package imports

---
+++ chapter-7/listing-7-1-auth-with-oauth2-service-providers/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/pat"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/facebook"
+	"github.com/markbates/goth/providers/twitter"
+)
