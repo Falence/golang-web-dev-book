c065274c160a126c5c348d171573e5d408cc9429
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-17 20:36:27 +0100

    Reads the login credentials, checks them and creates the JWT token

--- chapter-7/listing-7-2-jwt-token-based-auth/main.go
+++ chapter-7/listing-7-2-jwt-token-based-auth/main.go
@@ -46,3 +46,47 @@ func init() {
 		return
 	}
 }
+
+// Reads the login credentials, checks them and creates the JWT token
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	var user User
+
+	// Decode into User struct
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, "Error in request body")
+		return
+	}
+
+	// Validate user credentials
+	if user.UserName != "falence" && user.Password != "pass" {
+		w.WriteHeader(http.StatusForbidden)
+		fmt.Fprintln(w, "Wrong info")
+		return
+	}
+
+	// Create a signer for rsa 256
+	t := jwt.New(jwt.GetSigningMethod("RS256"))
+
+	// Set our claims
+	t.Claims["iss"] = "admin"
+	t.Claims["CustomUserInfo"] = struct {
+		Name string
+		Role string
+	}{user.UserName, "Member"}
+
+	// Set the expiration time
+	t.Claims["exp"] = time.Now().Add(time.Minute * 20).Unix()
+	tokenString, err := t.SignedString(signKey)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, "Sorry, error while signing token!")
+		log.Printf("Token signing error: %v\n", err)
+		return
+	}
+
+	// Respond
+	response := Token{tokenString}
+	jsonResponse(response, w)
+} 
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module web-dev-with-golang-book-by-shiju
 go 1.16
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gorilla/mux v1.6.2
 	github.com/gorilla/pat v1.0.1
 	github.com/markbates/goth v1.68.0
 )
