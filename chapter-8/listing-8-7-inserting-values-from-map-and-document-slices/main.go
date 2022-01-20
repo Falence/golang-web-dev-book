7e8d5c37d69358013576bb179d7d783c66ac6a95
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-20 20:27:08 +0100

    inserting values from a map and a document slice

---
+++ chapter-8/listing-8-7-inserting-values-from-map-and-document-slices/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+func main() {
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+
+	// Get collection
+	c := session.DB("taskdb").C("categories")
+
+	docM := map[string]string{
+		"name": "Open Source",
+		"description": "Tasks for open-source projects",
+	}
+
+	// Insert a map object
+	err = c.Insert(docM)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	docD := bson.D{
+		{"name", "Project"},
+		{"description", "Project Tasks"},
+	}
+
+	// Insert a document slice
+	err = c.Insert(docD)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
