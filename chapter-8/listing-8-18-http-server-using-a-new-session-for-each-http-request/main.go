fbc469a0120c5881e3c3608124a3579b7497dcd8
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-21 15:39:48 +0100

    creating methods for our DataStore type

--- chapter-8/listing-8-18-http-server-using-a-new-session-for-each-http-request/main.go
+++ chapter-8/listing-8-18-http-server-using-a-new-session-for-each-http-request/main.go
@@ -24,4 +24,20 @@ type (
 	}
 )
 
+// Close mgo.Session
+func (d *DataStore) Close() {
+	d.session.Close()
+}
 
+// Return a collection from the database
+func (d *DataStore) C(name string) *mgo.Collection {
+	return d.session.DB("taskdb").C(name)
+}
+
+// Create a new DataStore object for each HTTP request
+func NewDataStore() *DataStore {
+	ds := &DataStore{
+		session: session.Copy(),
+	}
+	return ds
+}
