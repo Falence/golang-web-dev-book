2d135be102ad6acd8f10f1b4b796da7c539406cc
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-30 21:17:40 +0100

    implement reverse string function

--- chapter-10/listing-10-1/stringutils/utils.go
+++ chapter-10/listing-10-1/stringutils/utils.go
@@ -17,3 +17,13 @@ func SwapCase(str string) string {
 	}
 	return buf.String()
 }
+
+
+// Reverse the string parameter
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r) - 1; i < len(r) / 2; i, j = i + 1, j - 1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
