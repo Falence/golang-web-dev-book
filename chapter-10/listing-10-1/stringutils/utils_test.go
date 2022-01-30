55e5465926980a58e6939a11d31f0a449f899c17
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-30 21:25:21 +0100

    implement test case for Reverse function

--- chapter-10/listing-10-1/stringutils/utils_test.go
+++ chapter-10/listing-10-1/stringutils/utils_test.go
@@ -13,3 +13,13 @@ func TestSwapCase(t *testing.T) {
 		t.Errorf("SwapCase(%q) == %q, expected %q", input, result, expected)
 	}
 }
+
+// Test case for the Reverse function
+func TestReverse(t *testing.T) {
+	input, expected := "Hello, World", "dlroW olleH"
+	result := Reverse(input)
+
+	if result != expected {
+		t.Errorf("Reverse(%q) == %q, expected %q", input, result, expected)
+	}
+}
