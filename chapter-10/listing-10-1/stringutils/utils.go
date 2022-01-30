cbde3e8ba6ba74c3fe947f28f384ae68e2872604
Falence Lemungoh <falencelemungoh@gmail.com>
2022-01-30 21:17:21 +0100

    implement swapCase function for strings

---
+++ chapter-10/listing-10-1/stringutils/utils.go
@@ -0,0 +1,19 @@
+package stringutils
+
+import (
+	"bytes"
+	"unicode"
+)
+
+// Swap the case of a string parameter
+func SwapCase(str string) string {
+	buf := &bytes.Buffer{}
+	for _, r := range str {
+		if unicode.IsUpper(r) {
+			buf.WriteRune(unicode.ToLower(r))
+		} else {
+			buf.WriteRune(unicode.ToUpper(r))
+		}
+	}
+	return buf.String()
+}
