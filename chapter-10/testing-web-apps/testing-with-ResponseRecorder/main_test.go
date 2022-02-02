843d1b389136e1be5d45b86d36cad0f37cd40ea2
Falence Lemungoh <falencelemungoh@gmail.com>
2022-02-02 15:18:06 +0100

    write test to create a user entity

--- chapter-10/testing-web-apps/testing-with-ResponseRecorder/main_test.go
+++ chapter-10/testing-web-apps/testing-with-ResponseRecorder/main_test.go
@@ -25,3 +25,25 @@ func TestGetUsers(t *testing.T) {
 		t.Errorf("HTTP status expected: 200, got: %d", w.Code)
 	}
 }
+
+// User Story - Users should be able to create a User entity
+func TestCreateUser(t *testing.T) {
+	r := mux.NewRouter()
+	r.HandleFunc("/users", createUser).Methods("POST")
+
+	userJson := `{"firstname": "Falence", "lastname": "Lemungoh", "email": "falence@lemu.com"}`
+	req, err := http.NewRequest(
+		"POST",
+		"/users",
+		strings.NewReader(userJson),
+	)
+	if err != nil {
+		t.Error(err)
+	}
+
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	if w.Code != 201 {
+		t.Errorf("HTTP status expected: 201, got: %d", w.Code)
+	}
+}
